467e9a31e2a6600b2d3e03b6b21cc34753e05ca8
Alex Pakalniskis <43630382+alex-pakalniskis@users.noreply.github.com>
2025-02-07 07:28:33 -0800

    Update ERC721Factory.sol

--- contracts/ERC721Factory.sol
+++ contracts/ERC721Factory.sol
@@ -1,33 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./SimpleERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ERC721Factory {
-    // Array to store the addresses of deployed SimpleERC721 contracts.
-    address[] public deployedContracts;
+    event CollectionCreated(address indexed collectionAddress, address indexed owner, string name, string symbol, string genre);
+    
+    address[] public collections;
 
-    event ContractDeployed(address indexed contractAddress);
-
-    /// @notice Deploys a new SimpleERC721 contract with the provided parameters.
-    /// @param name The name of the NFT collection.
-    /// @param symbol The symbol of the NFT collection.
-    /// @param genre The genre of the NFT collection.
-    /// @param location The location associated with the NFT collection.
-    function createERC721(
-        string memory name, 
-        string memory symbol, 
-        string memory genre, 
-        string memory location
-    ) external {
-        SimpleERC721 nftContract = new SimpleERC721(name, symbol, genre, location);
-        deployedContracts.push(address(nftContract));
-        emit ContractDeployed(address(nftContract));
+    function createCollection(string memory name, string memory symbol, string memory genre) external {
+        NFTCollection newCollection = new NFTCollection(name, symbol, genre, msg.sender);
+        collections.push(address(newCollection));
+        emit CollectionCreated(address(newCollection), msg.sender, name, symbol, genre);
     }
 
-    /// @notice Returns the list of deployed ERC721 contract addresses.
-    function getDeployedContracts() external view returns (address[] memory) {
-        return deployedContracts;
+    function getCollections() external view returns (address[] memory) {
+        return collections;
+    }
+}
+
+contract NFTCollection is ERC721, Ownable {
+    uint256 public nextTokenId;
+    string public genre;
+    mapping(uint256 => string) private tokenData;
+
+    event Minted(address indexed minter, uint256 indexed tokenId, string data);
+
+    constructor(string memory name, string memory symbol, string memory _genre, address owner)
+        ERC721(name, symbol)
+        Ownable(owner)
+    {
+        genre = _genre;
+    }
+
+    function mint(string memory data) external {
+        _safeMint(msg.sender, nextTokenId);
+        tokenData[nextTokenId] = data;
+        emit Minted(msg.sender, nextTokenId, data);
+        nextTokenId++;
+    }
+
+    function getTokenData(uint256 tokenId) external view returns (string memory) {
+        require(ownerOf(tokenId) != address(0), "Token does not exist");
+        return tokenData[tokenId];
     }
 }
 
